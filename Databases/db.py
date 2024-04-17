d9d5625f71bdbd3e4907d2479e80d73506432b77
bugradursun <72981010+bugradursun@users.noreply.github.com>
2024-04-17 10:37:37 +0300

    finish db op

--- Databases/db.py
+++ Databases/db.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+#https://www.youtube.com/watch?v=7lXTfS4Bbrg&list=PLjf6nsEcF5KMhk0ZmTQzzWtvoEHY84B8p&index=4
+#WE DONT WANT TO HAVE MULTIPLE CONNECTIONS TO OUR DATABASE, SO WE USE SINGLETON PATTERN
 def singleton(cls):
     instances = {}
 
@@ -10,6 +11,7 @@ def singleton(cls):
     
     return getinstance
 
+#use conn.commit if changes were made in database, if we just do get or select => no need to self.conn.commit
 class DatabaseDriver(object):
     #Database driver for the task app
     #Handles reading and writing data with the database.
@@ -40,3 +42,69 @@ class DatabaseDriver(object):
                 """     
             )
             self.conn.commit() # ?
+
+    def get_all_tasks(self):
+        cursor = self.conn.execute(
+            """
+
+            SELECT * FROM task;
+            """
+        )
+        tasks =[]
+        for row in cursor:
+            tasks.append(
+                {
+                    "id":row[0],
+                    "description":row[1],
+                    "done":bool(row[2])
+                }
+            )
+            return tasks
+        
+    def insert_task_table(self,description,done):
+        cur = self.conn.cursor()
+        cur.execute(
+            """
+            INSERT INTO task(description,done) VALUES(?,?);
+
+            """,
+            (description,done)
+        )
+        self.conn.commit()
+        return cur.lastrowid
+    
+    def get_task_by_id(self,id):
+        cur = self.conn.execute(
+            """
+            SELECT * FROM task WHERE id = ?;
+            """,
+            (id,) #even if we enter one parameter put , besides that like a two element tuple
+        )
+
+        for row in cur:
+            return {
+                "id":row[0],
+                "description":row[1],
+                "done":bool(row[2])
+            }
+        return None
+    
+    def update_task_by_id(self,id,description,done):
+        self.conn.execute(
+            """
+            UPDATE task SET description=?, done=? WHERE id=?;
+            """,
+            (description,done,id)
+        )
+        self.conn.commit()
+
+    
+    def delete_task_by_id(self,id):
+        self.conn.execute(
+            """
+            DELETE FROM task WHERE id=?;
+            """,
+            (id,)
+        )
+
+DatabaseDriver = singleton(DatabaseDriver) 
