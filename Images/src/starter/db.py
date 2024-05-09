28efb13d5e298a0b9b7695dc49995df7d1d8407c
bugradursun <72981010+bugradursun@users.noreply.github.com>
2024-05-09 11:00:33 +0300

    create and upload

--- Images/src/starter/db.py
+++ Images/src/starter/db.py
@@ -38,11 +38,57 @@ class Asset(db.Model):
         }
     
     def create(self,image_data):
+        """
+        Given an image in base64 encoding,does the following:
+        1.Reject image if not supported filename
+        2.Generate a random string for the image filename
+        3.Decodes the image and attemps to upload AWS
+        
+        """
         try:
             ext = guess_extension(guess_type(image_data)[0])[1:] #take the first element,slice from 1 index to end which gives us 'png' or 'gif' etc..
+
             if ext not in EXTENSIONS : 
                 raise Exception(f'Extension {ext} not supported!')
             
-            salt = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for i in range(16))
+            salt = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for i in range(16)) #random string is generated 
+
+            img_str = re.sub("^data:image/.+base64,","",image_data) #1)looking for data:image/ => 2) . means go to the end, + ; means go until ;base64. , and replace it with "",take from image_data
+            img_data = base64.b64decode(img_str)
+            img = Image.open(BytesIO(img_data))
+
+            self.base_url = S3_BASE_URL
+            self.salt = salt 
+            self.extension =ext
+            self.height = img.height
+            self.width = img.width
+            self.created_at = datetime.datetime.now()
+
+            img_filename = f'{salt}.{ext}'
+            self.upload(img,img_filename)
+
         except Exception as e:
             print('Error:',e)
+
+    def upload(self,img,img_filename):
+        """
+        Upload the image into S3 bucket
+        """
+
+        try:
+            img_temploc =f'{BASE_DIR}/{img_filename}'
+            img.save(img_temploc)
+
+            #upload image into S3 bucket
+            s3_client = boto3.client("s3") #aws connection
+            s3_client.upload_file(img_temploc,S3_BUCKET,img_filename)
+
+            s3_resource = boto3.resource("s3") #resource for s3
+            object_acl = s3_resource.ObjectACL(S3_BUCKET,img_filename)
+            object_acl.put(ACL ="public-read") #now anybody can read our image url
+
+            #remove img from temp loc
+            os.remove(img_temploc)
+
+        except Exception as e:
+            print('Error while uploading',e)
